38c02860a86291218f47277d562e555640b86764
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-05 22:26:08 +0000

    Brute Force prob 7

--- Arrays & Hashing.py
+++ Arrays & Hashing.py
@@ -265,5 +265,54 @@ class Solution:
 
         return out
 
-nums = [1,2,4,6]
-print(Solution().productExceptSelf(nums))
+# nums = [1,2,4,6]
+# print(Solution().productExceptSelf(nums))
+
+#### 7) Valid Sudoku
+
+# 7.1) BF - loop through columns, rows and boxes and check duplicates using hashmap
+# O(n^2)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == ".":
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square//3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
+
+board = [["1","2",".",".","3",".",".",".","."],
+ ["4",".",".","5",".",".",".",".","."],
+ [".","9","8",".",".",".",".",".","3"],
+ ["5",".",".",".","6",".",".",".","4"],
+ [".",".",".","8",".","3",".",".","5"],
+ ["7",".",".",".","2",".",".",".","6"],
+ [".",".",".",".",".",".","2",".","."],
+ [".",".",".","4","1","9",".",".","8"],
+ [".",".",".",".","8",".",".","7","9"]]
+
+print(Solution().isValidSudoku(board))
