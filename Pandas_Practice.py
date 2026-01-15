9ecea1c5a7d7071b7743292854330118a08c29d8
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-15 19:43:47 +0000

    595 solved - usage

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 ### 595) Big Countries
 
+data = { "name": ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola"],
+         "continent": ["Asia", "Europe", "Africa", "Europe", "Africa"],
+         "area": [652230, 28748, 2381741, 468, 1246700],
+         "population": [25500100, 2831741, 37100000, 78115, 20609294],
+         "gdp": [20343000000, 12960000000, 188681000000, 3712000000, 100990000000] }
+world = pd.DataFrame(data)
+
 # time O(n) Bolean filter complexity
 # space O(n) Bolean Mask time complexity
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
@@ -11,3 +18,4 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+print(big_countries(world))
