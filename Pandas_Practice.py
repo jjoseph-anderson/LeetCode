e965500699e4677009a8a7370dd0ec2cf59b82ec
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-18 22:54:03 +0000

    1683 solved - opt

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -92,4 +92,12 @@ def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
 
     return df[['tweet_id']]
 
+# print(invalid_tweets(tweets))
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+
+    invalid = tweets[tweets['content'].str.len() > 15]
+
+    return invalid[['tweet_id']]
+
 print(invalid_tweets(tweets))
