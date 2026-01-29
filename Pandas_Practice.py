a10e399cf97b5473504525b869fb4a5c8c051f74
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-29 19:21:01 +0000

    1050 solved

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -446,3 +446,12 @@ actor_director = pd.DataFrame({ "actor_id": [1, 1, 1, 1, 1, 2, 2],
                     "director_id":[1, 1, 1, 2, 2, 1, 1],
                     "timestamp": [0, 1, 2, 3, 4, 5, 6] })
 
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(by = ['actor_id', 'director_id'], as_index = False).count()
+
+    df = df[df.timestamp >= 3]
+
+    return df[['actor_id', 'director_id']]
+
+print(actors_and_directors(actor_director))
+
